3ad3f2ea9a4e37e253af7ec3e567193ef6c29c81
Indranil Bandyopadhyay <147315164+indranil143@users.noreply.github.com>
2025-04-25 20:44:35 +0530

    Update game.py

--- game.py
+++ game.py
@@ -18,16 +18,21 @@ BLACK = (0, 0, 0)
 class TicTacToe:
     def __init__(self, game):
         self.game = game
-        self.field_image = self.get_scaled_image(path='resources/field.png', res=[WIN_SIZE] * 2)
-        self.O_image = self.get_scaled_image(path='resources/o.png', res=[CELL_SIZE] * 2)
-        self.X_image = self.get_scaled_image(path='resources/x.png', res=[CELL_SIZE] * 2)
+        self.field_image = self.get_scaled_image('resources/field.png', [WIN_SIZE] * 2)
+        self.O_image = self.get_scaled_image('resources/o.png', [CELL_SIZE] * 2)
+        self.X_image = self.get_scaled_image('resources/x.png', [CELL_SIZE] * 2)
+
+        # faded versions
+        self.O_faded = self.O_image.copy()
+        self.O_faded.set_alpha(100)
+        self.X_faded = self.X_image.copy()
+        self.X_faded.set_alpha(100)
 
         self.game_array = [[INF, INF, INF],
                            [INF, INF, INF],
                            [INF, INF, INF]]
-        self.player = randint(0, 1)  # Randomly decide who starts
+        self.player = randint(0, 1)
 
-        # Predefined line combinations to check for winners
         self.line_indices_array = [
             [(0, 0), (0, 1), (0, 2)],
             [(1, 0), (1, 1), (1, 2)],
@@ -38,30 +43,47 @@ class TicTacToe:
             [(0, 0), (1, 1), (2, 2)],
             [(0, 2), (1, 1), (2, 0)]
         ]
+
         self.winner = None
+        self.winner_line = None
         self.game_steps = 0
         self.font = pg.font.SysFont('Verdana', CELL_SIZE // 4, True)
         self.game_over_sound = pg.mixer.Sound("resources/game_over.wav")
         self.move_sound = pg.mixer.Sound("resources/move.wav")
 
+        self.move_history = {0: [], 1: []}  # 0 for O, 1 for X
+
     def check_winner(self):
-        for line_indices in self.line_indices_array:
-            sum_line = sum([self.game_array[i][j] for i, j in line_indices])
-            if sum_line in {0, 3}:  # Check if X or O won
-                self.winner = 'XO'[sum_line == 0]
+        for line in self.line_indices_array:
+            values = [self.game_array[i][j] for i, j in line]
+            if values.count(0) == 3:
+                self.winner = 'O'
+            elif values.count(1) == 3:
+                self.winner = 'X'
+            if self.winner:
                 self.winner_line = [
-                    vec2(line_indices[0][::-1]) * CELL_SIZE + CELL_CENTER,
-                    vec2(line_indices[2][::-1]) * CELL_SIZE + CELL_CENTER
+                    vec2(line[0][::-1]) * CELL_SIZE + CELL_CENTER,
+                    vec2(line[2][::-1]) * CELL_SIZE + CELL_CENTER
                 ]
                 self.game_over_sound.play()
+                break
 
     def run_game_process(self):
+        if self.winner:
+            return
+
         current_cell = vec2(pg.mouse.get_pos()) // CELL_SIZE
         col, row = map(int, current_cell)
         left_click = pg.mouse.get_pressed()[0]
 
-        if left_click and self.game_array[row][col] == INF and not self.winner:
+        if left_click and self.game_array[row][col] == INF:
             self.game_array[row][col] = self.player
+            self.move_history[self.player].append((row, col))
+
+            if len(self.move_history[self.player]) > 3:
+                old_row, old_col = self.move_history[self.player].pop(0)
+                self.game_array[old_row][old_col] = INF  # Remove oldest
+
             self.move_sound.play()
             self.player = not self.player
             self.game_steps += 1
@@ -69,9 +91,17 @@ class TicTacToe:
 
     def draw_objects(self):
         for y, row in enumerate(self.game_array):
-            for x, obj in enumerate(row):
-                if obj != INF:
-                    self.game.screen.blit(self.X_image if obj else self.O_image, vec2(x, y) * CELL_SIZE)
+            for x, cell in enumerate(row):
+                if cell == 0 or cell == 1:
+                    img = self.O_image if cell == 0 else self.X_image
+                    faded_img = self.O_faded if cell == 0 else self.X_faded
+                    pos = vec2(x, y) * CELL_SIZE
+
+                    # Check if faded
+                    if (y, x) == self.move_history[cell][0] and len(self.move_history[cell]) == 3:
+                        self.game.screen.blit(faded_img, pos)
+                    else:
+                        self.game.screen.blit(img, pos)
 
     def draw_winner(self):
         if self.winner:
@@ -86,7 +116,7 @@ class TicTacToe:
 
     @staticmethod
     def get_scaled_image(path, res):
-        img = pg.image.load(path)
+        img = pg.image.load(path).convert_alpha()
         return pg.transform.smoothscale(img, res)
 
     def print_caption(self):
@@ -106,7 +136,7 @@ class TicTacToe:
 class Game:
     def __init__(self):
         pg.init()
-        pg.mixer.init()  # Initialize sound mixer
+        pg.mixer.init()
         self.screen = pg.display.set_mode([WIN_SIZE] * 2)
         self.clock = pg.time.Clock()
         self.tic_tac_toe = TicTacToe(self)
@@ -119,9 +149,8 @@ class Game:
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    self.new_game()
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                self.new_game()
 
     def run(self):
         while True:
@@ -131,7 +160,7 @@ class Game:
             self.clock.tick(60)
 
 
-# Main program
+# Main
 if __name__ == '__main__':
     game = Game()
     game.run()
